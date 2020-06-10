a109beca3a276bfb02b75ac5ce457056685cd5cb
Zoe Keller <keller.zoe98@gmail.com>
2020-06-10 23:06:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,14 +21,14 @@ class School
     end 
   end 
   
-school.roster
-school.add_student("Zach Morris", 9)
-school.roster
-school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-school.roster
-school.grade(9)
-school.sort
+School.roster
+School.add_student("Zach Morris", 9)
+School.roster
+School.add_student("AC Slater", 9)
+School.add_student("Kelly Kapowski", 10)
+School.add_student("Screech", 11)
+School.roster
+School.grade(9)
+School.sort
 
 end 
